d76dc26a78b5a1508ab4d2b04e4befd4ce0ade16
Flavio Gouvea <46804396+freitasgouvea@users.noreply.github.com>
2021-10-07 21:41:13 -0300

    criação do cartao.sol

---
+++ cartao.sol
@@ -0,0 +1,9 @@
+pragma solidity 0.8.9; 
+
+contract CartaoDeVacinacao {
+    //autor: Flavio
+    string constant public nome = "Flavio Neto";
+    uint lote = 100;
+    bool public imunizado = false;
+    address private wallet = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
+}
