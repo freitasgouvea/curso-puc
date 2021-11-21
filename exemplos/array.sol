c2ae802cc9ff985327c22f4252398fbd9835b12a
Flavio Gouvea <46804396+freitasgouvea@users.noreply.github.com>
2021-11-21 18:46:55 -0300

    Update array.sol

--- exemplos/array.sol
+++ exemplos/array.sol
@@ -10,20 +10,23 @@ contract CarteiraDeVacina {
     uint data;
   }
   
-  Vacina[] public arrayDeVacina;
+  Vacina[] public arrayDeVacina; // o array salva dados e objetos em sequencia. o index comeca no zero: array[0], array[1], array[2]...
   
   function adicionarVacina(string memory _tipo) public {
-    arrayDeVacina.push(Vacina(arrayDeVacina.length, _tipo, block.timestamp));
+    arrayDeVacina.push(Vacina(arrayDeVacina.length, _tipo, block.timestamp)); // array.push = adiciona um membro ao array
   }
   
   function quantidadeTotalDeVacinas() public view returns(uint) {
-     return (arrayDeVacina.length);
+     return (arrayDeVacina.length); // array.length = retorna a quantidade de membros do array
   }
   
   function quantidadeDeDoses(string memory _tipo) public view returns(uint){
       uint quantidade;
+      // for loop: o codigo e executado em loop passando por cada um dos membros do array for (inicializador; condicao; finalizador)
       uint i;
       for(i = 0; i < arrayDeVacina.length; i ++) {
+          // o solidity nao consegue comparar strings. para isso é necessario gerar um hash com a funcao criptografica keccak256 
+          // e traduzir a string com o abi.encodePacked
           if (keccak256(abi.encodePacked(arrayDeVacina[i].tipo)) == keccak256(abi.encodePacked(_tipo))) {
               quantidade++;
           }
