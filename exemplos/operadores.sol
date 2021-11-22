631b2cf41d67c97a6c37f396953d7b852ce85060
Flavio Gouvea <46804396+freitasgouvea@users.noreply.github.com>
2021-11-22 09:47:50 -0300

    Update operadores.sol

--- exemplos/operadores.sol
+++ exemplos/operadores.sol
@@ -2,14 +2,16 @@ pragma solidity 0.8.9;
 
 contract MeuContrato {
     
-    uint public variavel = 10;
+    uint public variavel = 100;
     
     function Somar(uint _valor) public {
         variavel = variavel + _valor;
+        // variavel += _valor;
     }
     
     function Subtrair(uint _valor) public {
         variavel = variavel - _valor;
+        // variavel -= _valor;
     }
     
     function Multiplicar(uint _valor) public {
@@ -18,6 +20,10 @@ contract MeuContrato {
     
     function Dividir(uint _valor) public {
         variavel = variavel / _valor;
+    }
+    
+    function Porcentagem(_valor) public {
+        (100 * _valor) / variavel;
     }
     
 }
